209941a6c07e2ec3a49ab2ee41f057420601ee37
banana0229 <130279769+banana0229@users.noreply.github.com>
2026-03-15 13:31:03 +0900

    test

--- js/multilanguage.js
+++ js/multilanguage.js
@@ -21,7 +21,45 @@ var testBasicActionDict = {};
 var Language = 0;
 
 
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // 只攔截 .enc 檔案
+  if (url.pathname.endsWith('.enc')) {
+    event.respondWith((async () => {
+      const res = await fetch(url.pathname);
+      const buffer = await res.arrayBuffer();
+      const data = new Uint8Array(buffer);
+      
+      // XOR 解密
+      const key = 0xAA;
+      for (let i = 0; i < data.length; i++) data[i] ^= key;
+
+      // 回傳正確 MIME
+      let mime = 'audio/mpeg';
+      if (url.pathname.endsWith('.ogg.enc')) mime = 'audio/ogg';
+      else if (url.pathname.endsWith('.wav.enc')) mime = 'audio/wav';
+      else if (url.pathname.endsWith('.flac.enc')) mime = 'audio/flac';
+
+      return new Response(data, { headers: { 'Content-Type': mime } });
+    })());
+  }
+});
+let this_bgm = new Audio();
+
 function Test() {
+
+    const file_name = "Integral_One.mp3.enc"
+
+    this_bgm.loop = true;
+    this_bgm.addEventListener("loadedmetadata", async () => {
+      this_bgm.play();
+    });
+    this_bgm.src = file_name;
+    console.log('play:' + file_name);
+    
+    return;
+
     TranslateToZh();
     
     let basicActionData = GetBasicActionData(1);
