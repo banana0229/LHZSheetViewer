c8ff1039cd96fe6dc31f82f0ed96319c330219c1
banana0229 <130279769+banana0229@users.noreply.github.com>
2026-03-15 14:51:25 +0900

    sw改模組化

--- js/multilanguage.js
+++ js/multilanguage.js
@@ -21,18 +21,15 @@ var testBasicActionDict = {};
 var Language = 0;
 
 if ('serviceWorker' in navigator) {
-
     const currentPath = window.location.pathname;
     // 取當前目錄作為 scope
     const dirPath = currentPath.substring(0, currentPath.lastIndexOf('/') + 1);
-    console.log(dirPath);
-    navigator.serviceWorker.register('./sw.js', { scope: dirPath })
+    navigator.serviceWorker.register('./sw.js', { type: 'module', scope: dirPath })
     .then(reg => {
       console.log('Service Worker registered:', reg);
-
-      // 等 SW 完全激活
       return navigator.serviceWorker.ready;
     })
+    .then(() => console.log('SW active and ready'))
     .catch(err => console.error('SW registration failed:', err));
 }
 
