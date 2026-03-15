e2d9db0ae0d2b7af6a7c8050757211fd4f573501
banana0229 <130279769+banana0229@users.noreply.github.com>
2026-03-15 14:12:51 +0900

    debug log

--- sw.js
+++ sw.js
@@ -2,12 +2,15 @@
 const key = 0xAA;
 
 self.addEventListener('fetch', event => {
+  console.log('fetch:'+event.request.url);
   const url = new URL(event.request.url);
 
   // 只攔截 .enc 檔案
   if (url.pathname.endsWith('.enc')) {
+    console.log('is enc file try to fetch');
     event.respondWith((async () => {
       const res = await fetch(url.pathname);
+      console.log('fetch done, try to decrypt');
       const buffer = await res.arrayBuffer();
       const data = new Uint8Array(buffer);
       
